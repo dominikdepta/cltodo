219d8f7f7103ba32f451a8b8966a2b5d17c6aca9
Dominik Depta <dominikdepta@gmail.com>
2025-04-11 10:14:55 +0200

    extract HelpContainer

--- src/modules/app/HelpView.tsx
+++ src/modules/app/HelpView.tsx
@@ -1,20 +1,6 @@
-import { Box } from "ink";
 import React from "react";
-import { Footer } from "../../components/Footer/Footer.tsx";
-import { Header } from "../../components/Header/Header.tsx";
-import { Key } from "../../components/Key/Key.tsx";
-import { Help } from "../help/Help.tsx";
+import { HelpContainer } from "../help/HelpContainer/HelpContainer.tsx";
 
 export const HelpView = () => {
-  return (
-    <Box flexDirection="column">
-      <Header title="Help" />
-
-      <Help />
-
-      <Footer>
-        <Key value="l" label="todo list" variant="secondary" />
-      </Footer>
-    </Box>
-  );
+  return <HelpContainer />;
 };
---
+++ src/modules/help/HelpContainer/HelpContainer.tsx
@@ -0,0 +1,37 @@
+import { Box } from "ink";
+import React from "react";
+import { Footer } from "../../../components/Footer/Footer.tsx";
+import { Header } from "../../../components/Header/Header.tsx";
+import { HelpKey } from "../HelpKey/HelpKey.tsx";
+import { HelpTitle } from "../HelpTitle/HelpTitle.tsx";
+import { Key } from "../../../components/Key/Key.tsx";
+
+export const HelpContainer = () => {
+  return (
+    <Box flexDirection="column">
+      <Header title="Help" />
+
+      <Box flexDirection="column" gap={1}>
+        <Box flexDirection="column">
+          <HelpTitle>General</HelpTitle>
+          <HelpKey value="q" label="quit" />
+          <HelpKey value="h" label="help" />
+          <HelpKey value="l" label="todo list" />
+        </Box>
+
+        <Box flexDirection="column">
+          <HelpTitle>Todo list</HelpTitle>
+          <HelpKey value="s" label="search" />
+          <HelpKey value="e" label="edit" />
+          <HelpKey value="space" label="toggle" />
+          <HelpKey value="↑ k" label="up" />
+          <HelpKey value="↓ j" label="down" />
+        </Box>
+      </Box>
+
+      <Footer>
+        <Key value="l" label="todo list" variant="secondary" />
+      </Footer>
+    </Box>
+  );
+};
