f7cefe9e9cc3018ec7d4f82eb1af88b58e4744ab
Dominik Depta <dominikdepta@gmail.com>
2025-04-11 12:29:58 +0200

    replace icon with symbol in Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,10 +3,14 @@ import TextInput from "ink-text-input";
 import React from "react";
 import { InputProps } from "./types.ts";
 
-export const Input = ({ icon, ...inputProps }: InputProps) => {
+export const Input = ({ symbol, ...inputProps }: InputProps) => {
   return (
-    <Box flexDirection="row" gap={1}>
-      {icon && <Text>{icon}</Text>}
+    <Box flexDirection="row">
+      {symbol && (
+        <Box marginRight={1}>
+          <Text backgroundColor="blue" color="black">{` ${symbol} `}</Text>
+        </Box>
+      )}
       <TextInput {...inputProps} />
     </Box>
   );
--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -2,5 +2,5 @@ import TextInput from "ink-text-input";
 import React from "react";
 
 export interface InputProps extends React.ComponentProps<typeof TextInput> {
-  icon?: React.ReactNode;
+  symbol?: string;
 }
